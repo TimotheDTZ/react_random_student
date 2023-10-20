13f7de5334c31207714e7c55c923ac168382b541
tides <tdeschildre@adexos.fr>
2023-10-20 11:33:24 +0200

    create participants list

---
+++ src/components/participants/participant.js
@@ -0,0 +1,14 @@
+export var Participants = [
+    { nom: "Aurore", genre: "Fille" },
+    { nom: "Shaima", genre: "Fille" },
+    { nom: "Coraly", genre: "Fille" },
+    { nom: "Clothilde", genre: "Fille" },
+    { nom: "Camille", genre: "Fille" },
+    { nom: "Thomas", genre: "Garçon" },
+    { nom: "Borris", genre: "Garçon" },
+    { nom: "Pierre", genre: "Garçon" },
+    { nom: "Maxence", genre: "Garçon" },
+    { nom: "Timothe", genre: "Garçon" },
+    { nom: "Lohan", genre: "Garçon" },
+    { nom: "Obryan", genre: "Garçon" },
+];
