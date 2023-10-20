8114e2f383a647d51bcbea1afecaa355872f3ebe
tides <tdeschildre@adexos.fr>
2023-10-20 12:16:44 +0200

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            Made with 🫶️ by <strong>Timothé</strong>
+        </footer>
+    );
+}
+
+export default Footer;
